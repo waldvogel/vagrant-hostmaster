bc967934bdce888dfc1cd88950bd7ca48a2225ae
S. Brent Faulkner <brent.faulkner@mosaic.com>
2012-08-18 20:47:27 -0400

    don't process vms that haven't been created yet

--- lib/vagrant/hostmaster/command/base.rb
+++ lib/vagrant/hostmaster/command/base.rb
@@ -14,7 +14,11 @@ module Vagrant
           return if !argv
 
           with_target_vms(argv, options) do |vm|
-            Hostmaster::VM.new(vm).send sub_command.to_sym
+            if vm.created?
+              Hostmaster::VM.new(vm).send sub_command.to_sym
+            else
+              vm.ui.info I18n.t("vagrant.commands.common.vm_not_created")
+            end
           end
 
           # Success, exit status 0
